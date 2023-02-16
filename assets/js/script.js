134bb95c7e6fa1b7fec317b22679a6a96717e304
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-02-16 18:31:14 -0500

    Created repo, added pseudo code in JavaScript, and asset folders

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Implement a timer
+// Build the home screen
+    // HTML box/template for the javascript content
+    // Create an input box for players initials
+// Build the question view w/ static data
+// Build high score view
+    // js
+// Link things together 
+    // Click to view high scores, show questions
+// Create an array of questions
+// Write function to show a question based on an index
+// Show 5 questions with answers squencially
+// Keep the finals score of the quiz
+    // js
+    // Have a page that shows high scores
+// On that page have a clear button to clear high schools
+// Add the final score to local storage
+// Clear all scores in local storage
+
+// Hover on answer boxes
+// Show after click if the answer is right or wrong
+// track the right and wrong answers 
+// Track with score
