02f676467f443dcb8f540d23b5377d6ce03761fc
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-02-20 19:25:47 -0500

    Added timer start code, questions array, and quiz start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,82 @@
+// Questions Array
+var qarray = [
+  {
+    question: "What is JavaScript?",
+    answers: [
+      "a) JavaScript is a scripting language used to make the website interactive",
+      "b) JavaScript is an assembly language used to make the website interactive",
+      "c) JavaScript is a compiled language used to make the website interactive",
+      "d) None of the above",
+    ],
+    correct: "a) JavaScript is a scripting language used to make the website interactive",
+  },
+  {
+    question: "Which of the following is correct about JavaScript?",
+    answers: [
+      "a) JavaScript is an Object-Based language",
+      "b) JavaScript is Assembly-language",
+      "c) JavaScript is an Object-Oriented language",
+      "d) JavaScript is a High-level language",
+    ],
+    correct: "a) JavaScript is an Object-Based language",
+  },
+  {
+    question: "Which statement defines closures in JavaScript?",
+    answers: [
+      "a) JavaScript is a function that is enclosed with references to its inner function scope",
+      "b) JavaScript is a function that is enclosed with references to its lexical environment",
+      "c) JavaScript is a function that is enclosed with the object to its inner function scope",
+      "d) None of the above",
+    ],
+    correct: "b) JavaScript is a function that is enclosed with references to its lexical environment",
+  },
+  {
+    question: "Arrays in JavaScript are defined by which of the following statements?",
+    answers: [
+      "a) It is an ordered list of values",
+      "b) It is an ordered list of objects",
+      "c) It is an ordered list of string",
+      "d) It is an ordered list of functions",
+    ],
+    correct: "a) It is an ordered list of values",
+  },
+  {
+    question: "Which of the following can be used to call a JavaScript Code Snippet?",
+    answers: [
+      "a) RMI",
+      "b) Preprocessor",
+      "c) Triggering Event",
+      "d) Function/Method",
+    ],
+    correct: "d) Function/Method",
+  },
+]
+
+var playerTally = 0;
+var playerScore = 0;
+console.log(qarray[playerTally].question)
+// playerTally++
+// console.log(qarray[playerTally].question)
+
 // Timer
 
-// Add timer start 
+// Timer START on click
 
-// timeEl.addEventListener('click', function(){
-//   let i = 0;
-//   let timerId = setInterval(funtion() {
-//     console.log('!')
-//   }, 1000);
-// });
+var startbtn = document.querySelector(".startbtn");
+
+startbtn.addEventListener("click", function () {
+  setTime();
+  document.querySelector(".start-quiz").classList.add("hidden");
+  document.querySelector(".quiz").classList.remove("hidden");
+  display();
+});
 
 // Selects element by class
 var timeEl = document.querySelector(".time");
-
 // Selects element by id
 var mainEl = document.getElementById("main");
 
-var secondsLeft = 11;
+var secondsLeft = 15;
 
 function setTime() {
   var timerInterval = setInterval(function () {
@@ -33,38 +94,36 @@ function sendMessage() {
   timeEl.textContent = "Time's Up!";
 }
 
-setTime();
+// Function to show a question and answers based on an index
+function display() {
+  document.querySelector(".titleq").textContent = qarray[playerTally].question;
+  for (let i = 0; i < qarray[playerTally].answers.length; i++) {
+    const element = qarray[playerTally].answers[i];
+    document.querySelector(".input-answers").children[i].textContent = element
+  }
+}
 
-// Build the home screen
-// HTML box/template for the javascript content
 
-
-// Build the question view w/ static data
-// Build high score view
 
 // Click to view high scores, show initials/scores
 
 
-
-// Write function to show a question based on an index
-
 // Show 5 questions with answers squencially
 // Add up andwers as you go
-// var playerScore = ;
-// var playerTally = "";
+
 
 
 // Create an input box for players initials
 
-var formEl = $('#input-form');
-var initialsEl = $('input[name="Initials"]');
+var formEl = document.querySelector('#input-form');
+var initialsEl = document.querySelector('input[name="Initials"]');
 
 function handleFormSubmit(event) {
   event.preventDefault();
 
   console.log('Initials', initialsEl.val());
 }
-formEl.on('submit', handleFormSubmit);
+formEl.addEventListener('submit', handleFormSubmit);
 
 // track the right and wrong answers, keep the final score
 // Add the final score to local storage
@@ -76,6 +135,10 @@ formEl.on('submit', handleFormSubmit);
 // Hover on answer boxes
 
 // Show after click if the answer is right or wrong
+
+
+
+
 
 // Create an array of questions
 // QUESTIONS
